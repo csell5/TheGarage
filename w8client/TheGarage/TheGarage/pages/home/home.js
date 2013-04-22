75986dc0f9b0f8453801216c5e5860381879583d
Clark Sell <clark.sell@csell.net>
2013-04-22 13:19:16 -0500

    started adding the websocket connections

--- w8client/TheGarage/TheGarage/pages/home/home.js
+++ w8client/TheGarage/TheGarage/pages/home/home.js
@@ -5,25 +5,73 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+            initSignalR();
+        }
+    });
 
-            $("#Door").click(function () {
-                $('#Door').removeClass('doorClosed');
-                $('#Door').attr('class', 'doorOpen');
+    function initSignalR() {
+
+        var signalRServer = "http://10.0.0.45:18628",
+            hubName = "deviceCommunicationHub";
+
+        var connection = $.hubConnection(signalRServer);
+        var proxy = connection.createHubProxy(hubName);
+
+        connection.start().done(function () {
+
+            $("#InsideButton, #Inside").click(function () {
+                proxy.invoke("XX", "")
             });
 
             $("#OutsideButtonRight, #OutsideRight, #OutsideButtonLeft, #OutsideLeft").click(function () {
-                $('#OutsideRight, #OutsideLeft').removeClass('lightsOff');
-                $('#OutsideRight, #OutsideLeft').attr('class', 'lightsOn');
-
+                proxy.invoke("XX", "")
             });
 
-            $("#InsideButton, #Inside").click(function () {
-                $('#Inside').removeClass('lightsOff');
-                $('#Inside').attr('class', 'lightsOn');
+            $("#Door").click(function () {
+                proxy.invoke("XX", "")
             });
 
-        }
-    });
+        });
+
+
+        //DOOR
+        proxy.on('popNotification', function (msg) {
+            $('#Door').removeClass('doorClosed');
+            $('#Door').attr('class', 'doorOpen');
+        });
+
+        //Inside Lights toggle
+        proxy.on('XXX', function (id, cmd) {
+
+            switch(cmd)
+            {
+                case 'on':
+
+                    $('#Inside').removeClass('lightsOff');
+                    $('#Inside').attr('class', 'lightsOn');
+
+                    break;
+                case 'off':
+
+                    $('#Inside').removeClass('lightsOn');
+                    $('#Inside').attr('class', 'lightsOff');
+
+                    break;
+
+                default:
+                    //TODO: this should be RED for an ERROR
+                    $('#Inside').removeClass('lightsOn');
+                    $('#Inside').attr('class', 'lightsOff');
+            }
+
+        });
+
+        //OUTSIDE
+        proxy.on("", function (msg) {
+            $('#OutsideRight, #OutsideLeft').removeClass('lightsOff');
+            $('#OutsideRight, #OutsideLeft').attr('class', 'lightsOn');
+        });
+
+    }
 
 })();
