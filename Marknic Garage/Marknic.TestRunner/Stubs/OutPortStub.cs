e6818441f6921201e91508545bd0932d5b3faadf
marknic <nichols_ms@hotmail.com>
2013-07-14 08:12:50 -0500

    CSS updates

---
+++ Marknic Garage/Marknic.TestRunner/Stubs/OutPortStub.cs
@@ -0,0 +1,26 @@
+using System;
+using Marknic.NdGarageDoorLightsController;
+
+namespace Marknic.TestRunner.Stubs
+{
+    public class OutPortStub : IOutPort
+    {
+        private int _counter;
+
+        public void Write(bool state)
+        {
+            switch (_counter)
+            {
+                case 0:
+                    if (!state) throw new ApplicationException("Invalid Step - state should have been true.");
+                    _counter++;
+                    break;
+                    
+                case 1:
+                    if (state) throw new ApplicationException("Invalid Step - state should have been false.");
+                    _counter++;
+                    break;
+            }
+        }
+    }
+}
